dad9418ab5993a8371ebcff825d9cdfa9ed74de0
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-06-19 06:41:01 +0300

    Initial Commit

---
+++ CloserNumber/CloserNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloserNumberApp.swift
+//  CloserNumber
+//
+//  Created by Kasharin Mikhail on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CloserNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
